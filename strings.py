64a257bd3e222446e7928b7c7788d0cbf4895180
SMAUG\Pete <pwbrophy@gmail.com>
2021-07-26 20:36:38 +0100

    Test upload!

---
+++ strings.py
@@ -0,0 +1,18 @@
+print("Today is a good day to learn Python")
+print('Python is easy to learn')
+print("hello"+" world")
+
+line1 = "Number 1\tThe Larch"
+
+print(line1)
+
+line2 = "Number 2\tThe Horse Chesnut"
+
+print(line2)
+
+
+age = 44
+
+name = "peterface"
+
+print(name + f"chonker{age}")
